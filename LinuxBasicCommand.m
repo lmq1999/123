ce40587bbc035c8634e4a3cc7d442bac3bb8e3c1
lmq1999 <41988000+lmq1999@users.noreply.github.com>
2019-08-13 16:53:01 +0700

    Add files via upload

---
+++ LinuxBasicCommand.m
@@ -0,0 +1,31 @@
+# Linux basic command
+#### ls
+**ls**      : Hiển thị danh sách thư mục
+**ls -a**   : Hiển thị tất cả danh sách thư mục, bao gồm cả thư mục ẩn 
+**ls -l**   : Hiên thị chi tiết thông tin của các thư mục (không bao gồm thư mục ẩn )
+**ls -la**  :Hiển thị chi tiết thông t in của tất cả thư mục 
+#### cd 
+**pwd** Hiển thị đường dẫn từ root đến thư mục đang làm việc
+**cd <name>** Chuyển sang thư mục khác
+**cd ..** Quay lại 1 thư mục trước đó
+**cd** quay về home
+#### folder & file
+**mkdir <name>** Tạo thư mụcđó
+**mkdir <location>** Tạo thư mục tại vị trí khác
+**rm <name>** xóa têp tin/ thư mục
+**rmdir <name** xóa thư mục trống
+**cat <name>** đọc thư mục
+**cat > <name>** tạo thư mục 
+**cat <name1> <name2>  >  <name3>** ghép 2 thư mục sang thư mục thứ 3
+**mv <name> <location>** Chuyển file sang vị trí khác
+**mv <name> <name1>** Đổi tên file
+#### Other
+**man <command>**: Hiển thị manual của command đó
+**passwd** Đổi mật khẩu 
+**info (name)** Hiển thị thông tin của file 
+**apropos** Tìm kiếm thông tin về đối tượng 
+**history** Hiển thị các command đã sử dụng
+**sudo** Cho phép người dùng sử dụng với tư cách là superuser /root
+**clear** Dọn sạch cửa sổ terminal 
+
+
